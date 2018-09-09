0846426f86f4b2ebe155ef4a5c9ce80ef1200c78
chen jinhua <1325072515@qq.com>
2018-09-09 12:51:33 +0800

    观察员行为

--- Views/gcy/xwpj.js
+++ Views/gcy/xwpj.js
@@ -20,132 +20,162 @@ import Checkbox from '../component/xwCheckBox'
 import app from '../../app.json';
 import ModalDropdown from 'react-native-modal-dropdown'
 
+
 const deviceWidth = Dimensions.get('window').width;  
 const deviceheight = Dimensions.get('window').height;  
 const ds = new ListView.DataSource({
     rowHasChanged: (r1, r2) => r1 !== r2
 });
-export default class jtjh extends Component {
+
+export default class xwpj extends Component {
     constructor(props) {
         super(props);
      this.state = {
-            dataSource: ds.cloneWithRows([  ]),
-            xgzhs:[],
+            dataSource:ds.cloneWithRows([]),
+            dataCySource:[],
             userName:"",
             type:1,
             jtnc:"",
             xgzh:"",
             xwMc:"",
-            yd:""
+            ydTpe:"",
+            xwSocre:0,
+            realName:''
            
         }
     }
-
-    _remove(id){
-        fetch(app.Host+'api/behavior/Deletebehavior?id='+id)
-        .then((response) =>{
-          if(response.ok){
-            fetch(app.Host+'api/behavior/behaviorS?jtnc='+this.state.jtnc+"&loginName="+this.state.userName)
-            .then((response) =>{
-              if(response.ok){
-                return response.json();
-              }
-            })
-            .then((responseJson) => { 
-              let data=responseJson.data;
-              this.setState({dataSource:ds.cloneWithRows(data)})
-            })
-            .catch((error) => {
-              console.error(error); 
-            });
-          }
-        }).catch((error) => {
-          console.error(error); 
-        });
-    }
-
-
-
-    Save(){
-      
-      let url = app.Host+"api/behavior/addBehavior";  
-      let params ={
-          "jtnc":this.state.jtnc,
-          "xgzh":this.state.xgzh,
-          "xwMc":this.state.xwMc,
-          "yd":this.state.yd,
-          "creator":this.state.userName,
-      }; 
-       
-      fetch(url, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(params)
-    }).then((response) => {
-          if (response.ok) {
-            fetch(app.Host+'api/behavior/behaviorS?jtnc='+this.state.jtnc+"&loginName="+this.state.userName)
-            .then((response) =>{
-              if(response.ok){
-                return response.json();
-              }
-            })
-            .then((responseJson) => { 
-              let data=responseJson.data;
-              this.setState({dataSource:ds.cloneWithRows(data),type:1})
-            })
-            .catch((error) => {
-              console.error(error); 
-            });
-          }
-      }).catch((error) => {
-          console.error(error);
-      });
     
+
+ 
+
+
+  _rednerCy1()
+  {
+      return (this.state.dataCySource.map((t, i) =>this._rednerCy(t, i)))
+}
+
+
+_rednerCy(item,i){ 
+    let role=decodeURI(item.userRole)
+  return ( 
+  
+     <View  key = {i} 
+     style={{width:80,alignItems:'center'}}> 
+
+    <TouchableOpacity onPress={()=>{
+     this.setState({xgzh:decodeURI(item.userName),realName:decodeURI(item.realName)})
+    this._showDeatil()
+    }}>
+     <Image
+         source={role=="儿子"?require('../cygl/imgs/tx/boy.png'):require('../cygl/imgs/tx/girl.png')}
+         style={{
+         height: 50,
+         width: 50
+     }}
+         resizeMode='stretch'
+     ></Image>
+     <Text 
+          style={{
+          
+             width: 50,
+             fontSize:12,
+             textAlign:'center'
+         }} >{ decodeURI(item.realName)}</Text>
+     </TouchableOpacity>
+ </View>
+  )
+}
+
+
+_showDeatil(){
+  fetch(app.Host+'api/behavior/behaviorS?jtnc='+this.state.jtnc+"&gcy="+this.state.userName+"&xg="+this.state.xgzh)
+  .then((response) =>{
+    if(response.ok){
+      return response.json();
     }
+  })
+  .then((responseJson) => { 
+    let data=responseJson.data;
+    this.setState({dataSource:ds.cloneWithRows(data)})
+  })
+  .catch((error) => {
+    console.error(error); 
+  });
 
-    componentWillMount() {
-
-        AsyncStorage.getItem('user').then((item)=>{
-           return JSON.parse(item)
-             }).then((item)=>{ 
-                  this.setState({jtnc:decodeURI(item.nc),userName:decodeURI(item.userName)}) 
-                  fetch(app.Host+'api/behavior/behaviorS?jtnc='+item.nc+"&loginName="+decodeURI(item.userName))
-                  .then((response) =>{
-                    if(response.ok){
-                      return response.json();
-                    }
-                  })
-                  .then((responseJson) => { 
-                    let data=responseJson.data;
-                    this.setState({dataSource:ds.cloneWithRows(data)})
-                  })
-                  .catch((error) => {
-                    console.error(error); 
-                  });
+}
 
 
-                  fetch(app.Host+'api/behavior/xgs?jtnc='+item.nc)
-                  .then((response) =>{
-                    if(response.ok){
-                      return response.json();
-                    }
+_remove(id){
+  fetch(app.Host+'api/behavior/Deletebehavior?id='+id)
+  .then((response) =>{
+    if(response.ok){
+      this._showDeatil.bind(this)()
+    }
+  }).catch((error) => {
+    console.error(error); 
+  });
+}
+
+
+
+
+_save(){
+
+let url = app.Host+"api/behavior/addBehavior";  
+let params ={
+    "jtnc":this.state.jtnc,
+    "xgzh":this.state.xgzh,
+    "xwMc":this.state.xwMc,
+    "ydType":this.state.ydType,
+    "yd":this.state.xwSocre,
+    "creator":this.state.userName,
+}; 
+ 
+fetch(url, {
+method: 'POST',
+headers: {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+},
+body: JSON.stringify(params)
+}).then((response) => {
+    if (response.ok) {
+      this.setState({type:1})
+      this._showDeatil.bind(this)()
+      
+    }
+}).catch((error) => {
+    console.error(error);
+});
+}
+
+ componentWillMount(){
+
+      AsyncStorage.getItem('user').then((item)=>{
+          return JSON.parse(item)
+            }).then((item)=>{ 
+              
+              fetch(app.Host+'api/plans/xgSearch?jtnc='+decodeURI(item.nc))
+                 .then((response) =>{
+                   if(response.ok){
+                     return response.json();
+                   }
+                 })
+                 .then((responseJson) => { 
+                   let data=responseJson.data;
+                   this.setState({dataCySource:data,jtnc:decodeURI(item.nc),userName:decodeURI(item.userName)})
+                   this. _showDeatil.bind(this)()
                   })
-                  .then((responseJson) => { 
-                    let data=responseJson.data;
-                    data.forEach(element => {
-                        this.state.xgzhs.push(decodeURI(element.userName))
-                    });
-                    
-                    this.setState({xgzhs: this.state.xgzhs})
-                  })
-                  .catch((error) => {
-                    console.error(error); 
-                  });
-             })
-      }
+                 .catch((error) => {
+                   console.error(error); 
+                 });
+            }) 
+
+          
+  }
+
+
+   
 
 
     
@@ -175,13 +205,10 @@ export default class jtjh extends Component {
                     justifyContent:'center',
                     alignItems:'flex-end'}} 
                       onPress={()=>{
-                        if(this.props.tc){
-                          this.props.navigator.push({
-                              component:Main,
-                              })
-                        }else{
-                          this.props.navigator.jumpBack()
-                        }
+                        let  destRoute=this.props.navigator.getCurrentRoutes().find((item)=>{
+                          return item.id=="Main4"
+                        })
+                         this.props.navigator.popToRoute(destRoute);
                         }
                           }>
                         <Image source={require('./shyImage/back.png')}  
@@ -210,7 +237,19 @@ export default class jtjh extends Component {
                       </View> 
                   </View>
                  
-                
+                  <View
+                                style={{
+                                 height:100,
+                                justifyContent: 'flex-start',
+                                alignContent:'flex-start',
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                marginBottom: 10,
+                                marginTop: 10,
+                                flexWrap:'wrap'
+                            }}>
+                             {this._rednerCy1()}
+                             </View>
                 
                  <ListView
                                   dataSource={this.state.dataSource}
@@ -223,7 +262,7 @@ export default class jtjh extends Component {
                                             borderTopWidth:1,
                                             margin:5,
                                             borderRadius:10,
-                                            height:30}}>
+                                            height:50}}>
                                             <View style={{flex:1,
                                               justifyContent:'center',
                                               alignItems:'flex-start',
@@ -244,7 +283,15 @@ export default class jtjh extends Component {
                                           }}>
                                                 <Text style={{  fontSize:12, color:'#474747'}}>银豆 {rowData.yd}</Text>
                                             </View>
-                                            <View style={{flex:2,
+                                            <View style={{flex:1,
+                                              justifyContent:'center',
+                                              alignItems:'center'
+                                          }}>
+                                        
+                                            <Image source={rowData.ydType=="优"?require('./shyImage/you.png'):rowData.ydType=="良"?require('../shy/shyImage/lian.png'):require('../shy/shyImage/chai.png')} resizeMode='stretch' style={{height:20,width:20,marginLeft:10,marginRight:10}}></Image>
+                                 
+                                            </View>
+                                            <View style={{flex:1,
                                               justifyContent:'center',
                                               alignItems:'center'
                                           }}>
@@ -295,87 +342,60 @@ export default class jtjh extends Component {
                       <View style={{justifyContent:'center',alignItems:'center'}}>
                           <Text 
                           style={{fontSize:16,
-                            color:'#FFF',fontWeight:'bold'}}>行为添加</Text>
+                            color:'#FFF',fontWeight:'bold'}}>{this.state.realName}行为添加</Text>
                       </View> 
                       <View style={{marginRight:5,width:40}}> 
-                     <TouchableOpacity onPress={this.Save.bind(this)}>
+                     <TouchableOpacity onPress={this._save.bind(this)}>
                        <Text style={{color:'#FFFF00',fontSize:16}}>保存</Text>
                      </TouchableOpacity>
                       </View> 
                   </View>
-                 
-                  <View style={{flexDirection:'row',
+
+                           <View style={{flexDirection:'row',
                         borderBottomWidth:1,
                         borderBottomColor:'#F0F0F0',
-                        height:60,
+                        height:40,
                         alignItems:'center',
                         justifyContent:'space-between',
                         paddingLeft:20}}>      
                             
-                   <Text style={{flex:1}}>小鬼账号</Text>
-                   <ModalDropdown options={this.state.xgzhs}
-                                   
-                                        onSelect={(i,v)=>{
-                                             this.setState({xgzh:v})
-                                        }}
-                                        dropdownStyle={{width:150,fontSize:12}}
-                                        dropdownTextStyle={{fontSize:12}}
-                                        textStyle={{fontSize:12,justifyContent:'center'}}
-                                        style={{flex:2,justifyContent:'center',height:40}}/>
-                           
-                          
-                        
-                            
-                   </View>
 
-                   <View style={{flexDirection:'row',
-                        borderBottomWidth:1,
-                        borderBottomColor:'#F0F0F0',
-                        height:60,
-                        alignItems:'center',
-                        justifyContent:'space-between',
-                        paddingLeft:20}}>   
-                          <Text style={{flex:1}}>行为描述</Text>
-                        
-                        <TextInput 
-                           style={{flex:2}}
-                           underlineColorAndroid='transparent'
-                           placeholder='请输入行为描述'
-                           placeholderTextColor='black'
-                            onChangeText={(v)=>{
-
-                              this.setState({xwMc:v})
-                            }}>
-                           </TextInput>
-                            
-                            
-                           
-                   </View>
+                    <Text style={{flex:1}}>评价</Text>
+                    <ModalDropdown options={["优","良","差"]}
+                                  onSelect={(i,v)=>{
+                                       if(v=="优")
+                                        this.setState({ydType:v,xwSocre:100})
+                                        else if(v=="良")
+                                        this.setState({ydType:v,xwSocre:80})
+                                        else if(v=="差")
+                                        this.setState({ydType:v,xwSocre:60})
+                                   }}
+                                   defaultValue={'请选择'}
+                                   dropdownStyle={{width:150,fontSize:12}}
+                                   dropdownTextStyle={{fontSize:12}}
+                                   textStyle={{fontSize:12,justifyContent:'center'}}
+                                   style={{flex:2,justifyContent:'center',height:40}}/>
                   
-                 <View style={{flexDirection:'row',
-                        borderBottomWidth:1,
-                        borderBottomColor:'#F0F0F0',
-                        height:60,
-                        alignItems:'center',
-                        justifyContent:'space-between',
-                        paddingLeft:20}}>      
-                            
-
-                    <Text style={{flex:1}}>奖罚银豆数</Text>
-                              <TextInput 
-                           style={{flex:2}}
-                           underlineColorAndroid='transparent'
-                           placeholder='请输入奖罚银豆数'
-                           placeholderTextColor='black'
-                           onChangeText={(v)=>{
-                              this.setState({yd:v})
-                          }}>
-                           
-                          
-                           </TextInput>
-                            
                    </View>
-                  </View>
+                          <View style={{height:100,
+                              borderBottomWidth:1,
+                              borderBottomColor:'#F0F0F0',
+                        alignContent:'flex-start',
+                        justifyContent:'flex-start',
+                        marginLeft:20}}>
+               <TextInput 
+                multiline={true}
+                numberOfLines={5}  
+                style={{width:deviceWidth*0.98,height:100,textAlignVertical:'top',textAlign:'left'}}  
+                underlineColorAndroid='transparent'
+                placeholder={"请填写不少于10个字描述"}
+                onChangeText={(v)=>{
+                      this.setState({xwMc:v})
+                }}
+                ></TextInput>
+           </View>
+               
+                 </View>
                   )
       }
    }
